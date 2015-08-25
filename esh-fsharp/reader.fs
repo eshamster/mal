e502297918c14d4758b6aa254fee01833c025393
eshamster <hamgoostar@gmail.com>
2015-08-25 21:47:03 +0900

    Add read_str
    	modified:   reader.fs

--- esh-fsharp/reader.fs
+++ esh-fsharp/reader.fs
@@ -2,26 +2,30 @@ module Mal.Reader
 
 open System.Text.RegularExpressions
 
-type Reader() = class
-  member this.next = ()
-  member this.peek = ()
-  end
+type Reader(tokens : string list) = class
+  let mutable rest_tokens = tokens
 
-let private read_str = ()
-let private read_from = ()
+  member this.next : string =
+    let result = List.head rest_tokens
+    rest_tokens <- List.tail rest_tokens
+    result
+    
+  member this.peek : string =
+    List.head rest_tokens
+  end
 
 type private TokenAndRest = { token: string ; rest: string }
 
-let private tokenizer str =
+let private tokenizer (str:string) =
   let token_reg = "[\s,]*(~@|[\[\]{}()'`~^@]|\"(?:\\.|[^\\\"])*\"|;.*|[^\s\[\]{}('\"`,;)]*)"
   
   // TODO: Error Processing
-  let cut_down_by_regex str reg =
+  let cut_down_by_regex (str:string) (reg:string) =
     let m = Regex.Match(str, "^(" + reg + ")(.*$)")
     let values = [ for x in m.Groups -> x.Value ]
     { token = values.[1] ; rest = List.last values }
     
-  let rec rec_tokenizer result (rest:string) =
+  let rec rec_tokenizer (result:string list) (rest:string) =
     if rest.Length = 0 then
       result
     else
@@ -29,9 +33,15 @@ let private tokenizer str =
       rec_tokenizer (tr.token.Trim() :: result) tr.rest
   rec_tokenizer [] str |> List.rev
 
+let private read_str (str:string) : Reader = 
+  let tokens = tokenizer str
+  new Reader(tokens)
+
+let private read_from = ()
 let private read_list = ()
 let private read_atom = ()
 
 let test() =
-  tokenizer ",, ,,(12 13 1)" |> List.iter (fun token -> printfn "token: %s" token)
+  tokenizer ",, ,,(12 13 1)" |> List.iter (fun x -> printfn "%s" x)
+  let reader = read_str ",, ,,(12 13 1)"
   0
