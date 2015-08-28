1381a654971b16dd5faa79e4551576e5dad51580
eshamster <hamgoostar@gmail.com>
2015-08-28 21:44:22 +0900

    [Finished step32] Implement Env
    	modified:   env.fs
    	modified:   step3_env.fs

--- esh-fsharp/env.fs
+++ esh-fsharp/env.fs
@@ -4,11 +4,14 @@ open System.Collections.Generic;
 open Mal.Types
 
 type Env(its_outer:Env option) = class
-  let mutable outer = its_outer
+  let outer = its_outer
   let data = new Dictionary<string, MalType>()
 
   member this.Set (key:string) (value:MalType) : _ =
-    data.Add(key, value)
+    if data.ContainsKey key then
+      data.[key] <- value
+    else
+      data.Add(key, value)
 
   member this.Find (key:string) : Env option =
     if data.ContainsKey key then
@@ -18,8 +21,10 @@ type Env(its_outer:Env option) = class
         | Some x -> x.Find key
         | None -> Option<Env>.None
 
+  member this.Data() = data
+
   member this.Get (key:string) : MalType =
     match this.Find key with
-      | Some _ -> data.[key]
+      | Some x -> x.Data().[key]
       | None -> failwith ("The symbol is not defined: " + key)
   end
--- esh-fsharp/step3_env.fs
+++ esh-fsharp/step3_env.fs
@@ -24,7 +24,7 @@ let rec EVAL (data:MalType) (env:Env) : MalType =
     match ast with
       | :? MalSymbol as s ->
           let name : string = s.Get
-          repl_env.Get name
+          env.Get name
       | :? MalList as s ->
           let rec rec_evaled_list result (rest:MalType list) : MalType list =
             if rest.IsEmpty then
@@ -35,14 +35,34 @@ let rec EVAL (data:MalType) (env:Env) : MalType =
           new MalList(rec_evaled_list [] s.Get) :> _
       | _ -> ast
 
+  let set_symbol_to_env (list:MalType list) (env:Env) : MalType =
+    if list.Length < 2 then
+      failwith "SyntaxError: Symbol definition requires 2 parameters"
+    let key_obj = list.[0] :?> MalSymbol
+    let value = EVAL list.[1] env
+    env.Set key_obj.Get value
+    value
+
   let eval_def_ex (rest:MalType list) (env:Env) : MalType =
     if rest.Length <> 2 then
       failwith "SyntaxError: 'def!' requires 2 parameters"
-    
-    let key_obj = rest.[0] :?> MalSymbol
-    let value = eval_ast rest.[1] env
-    env.Set key_obj.Get value
-    value
+    set_symbol_to_env rest env
+
+  let eval_let_ast (rest:MalType list) (env:Env) : MalType =
+    let rec rec_set_to_env (defines:MalType list) (env:Env) : _ =
+      match defines.Length with
+        | 0 -> ()
+        | x when x % 2 = 0 ->
+            set_symbol_to_env defines env |> ignore
+            rec_set_to_env (defines |> List.tail |> List.tail) env
+        | _ -> failwith "SyntaxError: 'let*' requires even number parameters"
+    if rest.Length <> 2 then
+      failwith "SyntaxError: 'let*' requires 2 parameters"
+    if not (rest.[0] :? MalList) then
+      failwith ("SyntaxError: 'let*' requires a list as the definition of symbols: " + rest.[0].ToString)
+    let new_env = new Env(Some env)
+    rec_set_to_env (rest.[0] :?> MalList).Get new_env
+    EVAL rest.[1] new_env
       
   match data with
     | :? MalList as l ->
@@ -50,7 +70,7 @@ let rec EVAL (data:MalType) (env:Env) : MalType =
         | :? MalSymbol as s ->
           match s.Get with
             | "def!" -> eval_def_ex (List.tail l.Get) env
-            | "let*" -> failwith "Not implemented"
+            | "let*" -> eval_let_ast (List.tail l.Get) env
             | _ -> let list = (eval_ast l env :?> MalList).Get
                    (List.head list :?> MalFunc).Call (List.tail list)
         | _ -> let list = (eval_ast l env :?> MalList).Get
